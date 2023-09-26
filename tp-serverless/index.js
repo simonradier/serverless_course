6b0e0717bc779d6f8a58fa782722044e6c29491e
critik <simon.dev@outlook.fr>
2023-09-26 16:47:51 +0200

    Un peu de code pour utiliser notre BDD DynamoDB

--- tp-serverless/index.js
+++ tp-serverless/index.js
@@ -1,13 +1,24 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event.body,
-      },
-      null,
-      2
-    ),
-  };
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "crypto";
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+export const handler = async (event) => {
+	try {
+		const task = JSON.parse(event.body)
+		const command = new PutCommand({
+			TableName: process.env.TABLE_NAME,
+			Item: {
+				...task,
+				id: randomUUID()
+			}
+		})
+		const response = await docClient.send(command)
+		return {
+statusCode: 200,
+body: JSON.stringify(response, null, 2 )
+		};
+	} catch (error) {
+		return { statusCode: 500, body: error.message };
+	}
 };
